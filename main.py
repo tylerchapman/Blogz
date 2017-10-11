6b10250ae6df9ebe6da7d7f017f688b1ee87b3c4
chapmant <tylerchapman90@gmail.com>
2017-10-11 15:59:39 -0500

    added newpost functionality

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'QLciVE2R1J'
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+
+    blog_posts = Blog.query.all()
+    blog_posts = list(reversed(blog_posts))
+
+    return render_template('base.html', blog_posts=blog_posts)
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_post = Blog(title, body)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/blog')
+    else:
+        return render_template('newpost.html')
+
+
+if __name__ == '__main__':
+    app.run()
