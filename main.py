3b6aad14490de098cca29e2cc880414a3e33eb5e
chapmant <tylerchapman90@gmail.com>
2017-10-26 15:33:47 -0500

    Finished Assignment

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class User(db.Model):
 
 
 def logged_in():
-    if session.get('username') == True:
+    if session.get('username'):
         return True
     else:
         return False
@@ -44,7 +44,7 @@ def logged_in():
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup']
+    allowed_routes = ['login', 'signup', 'blog', 'index']
     if request.endpoint not in allowed_routes and 'username' not in session:
         redirect('/login')
 
@@ -62,6 +62,7 @@ def login():
             return redirect('/newpost')
         else:
             flash('User password incorrect, or user does not exist')
+            return redirect('/login')
     return render_template('login.html')
 
 
@@ -72,16 +73,19 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-
         existing_user = User.query.filter_by(username=username).first()
-        if not existing_user:
-            new_user = User(username, password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['username'] = username
-            return redirect('/blog')
+        if verify == password:
+            if not existing_user and len(username) > 3 and len(password) > 3:
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                return redirect('/newpost')
+            else:
+                flash('Duplicate user! Usernames and Passwords must be at least 3 characters long!')
+                return redirect('/signup')
         else:
-            flash('Duplicate User!')
+            flash('Passwords do not match!')
             return redirect('/signup')
     return render_template('signup.html')
 
@@ -124,18 +128,22 @@ def blog():
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
 
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        username = session['username']
-        owner = User.query.filter_by(username=username).first()
-        owner_id = owner.id
-        new_post = Blog(title, body, owner_id)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/blog')
+    if logged_in():
+        if request.method == 'POST':
+            title = request.form['title']
+            body = request.form['body']
+            username = session['username']
+            owner = User.query.filter_by(username=username).first()
+            owner_id = owner.id
+            new_post = Blog(title, body, owner_id)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect('/blog')
+        else:
+            return render_template('newpost.html')
     else:
-        return render_template('newpost.html')
+        flash("You must login to post!")
+        return redirect('/login')
 
 
 @app.route('/', methods=['GET'])
